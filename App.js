7cab321c811193a9b0a054a3a877860b33d634b2
0936308474 <phamvanchien984@gmail.com>
2021-03-16 09:19:03 +0700

    Bai 2

--- App.js
+++ App.js
@@ -1,27 +1,45 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, {Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
 
-// import React from 'react';
-// import { View, Text, ColorPropType } from 'react-native';
-// import { Colors } from 'react-native/Libraries/NewAppScreen';
-// // import {Avatar} from "./src/components/avatar";
+export default class App extends Component {
+  render() {
+    return (
+      <View style={{flex: 1}}>
+        <View style={[Style.viewFormat, {flex: 2.5, flexDirection:'column',alignItems:'stretch'}]}>
+          <View style={[Style.viewFormat, {flex: 1, backgroundColor:'#ff0000'}]}>
+            <Text style={Style.textFormat}>A</Text>
+          </View>
+          <View style={[Style.viewFormat, {flex: 1, backgroundColor:'#008000'}]}>
+            <Text style={Style.textFormat}>B</Text>
+          </View>
+          <View style={[Style.viewFormat, {flex: 1, backgroundColor:'#adff2f'}]}>
+            <Text style={Style.textFormat}>C</Text>
+          </View>
+          <View style={[Style.viewFormat, {flex: 1, backgroundColor:'#4169e1'}]}>
+            <Text style={Style.textFormat}>D</Text>
+          </View>
+        </View>
 
-// export default class App extends React.Component
-// {
-//   render(){
-//     return (
-//       <View>
-//       <Text style={{backgroundColor:'grey' }}>hello world hehe</Text>
-//       </View>
+        <View style={[Style.viewFormat, {flex: 3, flexDirection:'row',alignItems:'stretch'}]}>
+          <View style={[Style.viewFormat, {flex: 1, backgroundColor:'#ffff00'}]}>
+            <Text style={Style.textFormat}>A</Text>
+          </View>
+          <View style={[Style.viewFormat, {flex: 1, backgroundColor:'#a9a9a9'}]}>
+            <Text style={Style.textFormat}>B</Text>
+          </View>
+          <View style={[Style.viewFormat, {flex: 1, backgroundColor:'#da70d6'}]}>
+            <Text style={Style.textFormat}>C</Text>
+          </View>
+          <View style={[Style.viewFormat, {flex: 1, backgroundColor:'#800080'}]}>
+            <Text style={Style.textFormat}>D</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
 
-//       );
-//   }
-// }--------------------------
+/*--------------------------BAI 1 -------------------------------------
 import React, {Component} from 'react';
 import {Text, View, StyleSheet} from 'react-native';
 
@@ -31,34 +49,34 @@ export default class App extends Component {
       <View style={{flex: 1, flexDirection: 'column'}}>
         <View style={{flex: 1, flexDirection: 'row'}}>
           <View
-            style={
-              [Style.viewFormat,
+            style={[
+              Style.viewFormat,
               {
                 flex: 1,
                 backgroundColor: '#008000',
-              }]
-            }>
+              },
+            ]}>
             <Text style={Style.textFormat}>1</Text>
           </View>
           <View style={{flex: 4, flexDirection: 'column'}}>
             <View
-              style={
-                [Style.viewFormat,
+              style={[
+                Style.viewFormat,
                 {
                   flex: 1,
                   backgroundColor: '#ff0000',
-                }
-                ]}>
+                },
+              ]}>
               <Text style={Style.textFormat}>3</Text>
             </View>
             <View
-              style={
-                [Style.viewFormat,
+              style={[
+                Style.viewFormat,
                 {
                   flex: 1,
                   backgroundColor: '#f08080',
-                }]
-              }>
+                },
+              ]}>
               <Text style={Style.textFormat}>4</Text>
             </View>
           </View>
@@ -69,49 +87,44 @@ export default class App extends Component {
             flexDirection: 'row',
           }}>
           <View
-            style={
-              [Style.viewFormat,
+            style={[
+              Style.viewFormat,
               {
                 flex: 1,
                 backgroundColor: '#f0e68c',
-              }]
-            }>
+              },
+            ]}>
             <Text style={Style.textFormat}> 2</Text>
           </View>
           <View
-            style={
-              
-              {
-                flex: 2.5,
-                flexDirection:'column'  
-              }
-            }>
-            <View style={[Style.viewFormat,{flex: 1,
-                backgroundColor: '#1e90ff',}]}>
+            style={{
+              flex: 2.5,
+              flexDirection: 'column',
+            }}>
+            <View
+              style={[Style.viewFormat, {flex: 1, backgroundColor: '#1e90ff'}]}>
               <Text style={Style.textFormat}>5</Text>
             </View>
-            <View style={[Style.viewFormat,{flex: 1,
-                backgroundColor: '#8b008b',}]}>
+            <View
+              style={[Style.viewFormat, {flex: 1, backgroundColor: '#8b008b'}]}>
               <Text style={Style.textFormat}>6</Text>
             </View>
           </View>
           <View
-            style={
-              {
-                flex: 1.5,
-                flexDirection:'column'  
-              }
-            }>
-            <View style={[Style.viewFormat,{flex: 1,
-                backgroundColor: '#8b008b',}]}>
+            style={{
+              flex: 1.5,
+              flexDirection: 'column',
+            }}>
+            <View
+              style={[Style.viewFormat, {flex: 1, backgroundColor: '#8b008b'}]}>
               <Text style={Style.textFormat}>7</Text>
             </View>
-            <View style={[Style.viewFormat,{flex: 1,
-                backgroundColor: '#ff8c00',}]}>
+            <View
+              style={[Style.viewFormat, {flex: 1, backgroundColor: '#ff8c00'}]}>
               <Text style={Style.textFormat}>8</Text>
             </View>
-            <View style={[Style.viewFormat,{flex: 1,
-                backgroundColor: '#a9a9a9',}]}>
+            <View
+              style={[Style.viewFormat, {flex: 1, backgroundColor: '#a9a9a9'}]}>
               <Text style={Style.textFormat}>9</Text>
             </View>
           </View>
@@ -120,6 +133,7 @@ export default class App extends Component {
     );
   }
 }
+*/
 const Style = StyleSheet.create({
   textFormat: {
     fontSize: 25,
