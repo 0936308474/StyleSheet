6dc56d7331ced7b99d692c1790f723dd8c5c7a32
0936308474 <phamvanchien984@gmail.com>
2021-03-17 20:38:44 +0700

    toan tu 3 ngoi

--- src/Screens/MainScreen.js
+++ src/Screens/MainScreen.js
@@ -2,17 +2,18 @@ import React, { Component } from 'react'
 import { Text, View, SafeAreaView } from 'react-native'
 
 export default class MainScreen extends Component {
+
     render() {
 
         // const word={en:"one", vn:"Mot"}
-        const{en,vn} = {en:"one", vn:"Mot"};
+        const{en,vn,isMemory} = {en:"one", vn:"Mot", isMemory : true};
         return (
 
-            <SafeAreaView style={{flex:1}}>
+            <SafeAreaView style={{flex:1, flexDirection:'row', justifyContent:'space-between'}}>
                 {/* <Text> {word.en} </Text>
                 <Text> {word.vn} </Text> */}
                 <Text> {en} </Text>
-                <Text> {vn} </Text>
+                <Text> {isMemory ? "----": vn} </Text>
             </SafeAreaView>
         );
     }
