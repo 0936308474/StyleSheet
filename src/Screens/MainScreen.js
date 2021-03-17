694af73f90632236d705d7bfa49000f2556e4ef2
0936308474 <phamvanchien984@gmail.com>
2021-03-17 20:47:32 +0700

    hien thi danh sach mang ra man hinh

--- src/Screens/MainScreen.js
+++ src/Screens/MainScreen.js
@@ -7,13 +7,19 @@ export default class MainScreen extends Component {
 
         // const word={en:"one", vn:"Mot"}
         const{en,vn,isMemory} = {en:"one", vn:"Mot", isMemory : true};
+        const arr=['teo','ti','tun','tuan'];
         return (
 
-            <SafeAreaView style={{flex:1, flexDirection:'row', justifyContent:'space-between'}}>
+            <SafeAreaView style={{flex:1, flexDirection:'row', justifyContent:'space-evenly'}}>
                 {/* <Text> {word.en} </Text>
                 <Text> {word.vn} </Text> */}
-                <Text> {en} </Text>
-                <Text> {isMemory ? "----": vn} </Text>
+                {/* <Text> {en} </Text>
+                <Text> {isMemory ? "----": vn} </Text> */}
+                {
+                    arr.map((name)=>{
+                      return  <Text>{name}</Text>
+                    })
+                }
             </SafeAreaView>
         );
     }
